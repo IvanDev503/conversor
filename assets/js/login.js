a9405c74a8baee5a00bd038661219275d9465bbd
rubenitez <rubenitez@gmail.com>
2021-04-08 17:35:51 -0600

    probando seguridad con cookies

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,5 +1,32 @@
 var loggedIn = false;
 
+///////////// Funciones para cookies //////////////
+function setCookie(name,value,days) {
+  var expires = "";
+  if (days) {
+      var date = new Date();
+      date.setTime(date.getTime() + (days*24*60*60*1000));
+      expires = "; expires=" + date.toUTCString();
+  }
+  document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+function getCookie(name) {
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(';');
+  for(var i=0;i < ca.length;i++) {
+      var c = ca[i];
+      while (c.charAt(0)==' ') c = c.substring(1,c.length);
+      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+  }
+  return null;
+}
+function eraseCookie(name) {   
+  document.cookie = name +'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+}
+
+//////////////////////////////////////////////////////////////////////////////////////
+
+
 function authenticate() {
   var user = document.getElementById("user").value;
   var password = document.getElementById("password").value;
@@ -22,6 +49,7 @@ function login(user, password) {
       if(response.success === true){
         nextPage();
         //aqui poner cookie
+        setCookie('logcookie','testcookie',7);
       }
     },
     contentType: "application/json; charset=utf-8",
@@ -56,10 +84,10 @@ function handleLogout( ){
 //   }
 // });
 
-/* function ledIn() {
-  console.log(loggedIn);
-  if (loggedIn) {
-    window.locationf="https://www.google.com";
+// function ledIn() {
+//   console.log(loggedIn);
+//   if (loggedIn = true) {
+//     window.location="google.com";
  
-  }
-} */
+//   }
+// }
