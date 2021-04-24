8555b5ae9d17c232aca502b8e4e4b60fd5665126
rubenitez <rubenitez@gmail.com>
2021-04-24 12:10:34 -0600

    probando decodificador para utf-8

--- assets/js/scriptcv.js
+++ assets/js/scriptcv.js
@@ -35,7 +35,7 @@ function readmultifiles(files) {
       // do sth with text
       var li = document.createElement("li");
       var a = document.createElement("a");
-      var html =
+      /* var html =
         `
       <!DOCTYPE html>
       <html lang="en">
@@ -51,6 +51,26 @@ function readmultifiles(files) {
         document.write(atob('` +
         b64 + 
         `'));
+      </script>
+      </html>
+      `; */
+
+      var html =
+        `
+      <!DOCTYPE html>
+      <html lang="en">
+      <head>
+          <meta charset="UTF-8">
+          <meta http-equiv="X-UA-Compatible" content="IE=edge">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>Document</title>
+      </head>
+      <body>
+      </body>
+      <script>
+        document.write(decodeURIComponent(escape(atob('` +
+        b64 + 
+        `'))));
       </script>
       </html>
       `;
